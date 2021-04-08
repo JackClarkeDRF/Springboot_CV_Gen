dce44b9a96395ab16b026d4619dfa693bfa156f0
Jack Clarke <u6672568@anu.edu.au>
2021-04-08 13:20:20 +1000

    first commit

---
+++ src/test/java/org/example/controller/ControllerIntegrationTest.java
@@ -0,0 +1,28 @@
+package org.example.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void getAllResumes() {
+    }
+
+    @Test
+    public void getResumeByID() {
+    }
+
+    @Test
+    public void deleteResume() {
+    }
+
+    @Test
+    public void addResume() {
+    }
+
+    @Test
+    public void updateResume() {
+    }
+}
